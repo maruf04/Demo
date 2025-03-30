b6a361e66056a8ce5acd5338d8548016370e9812
Maruf <marufxhasan04@gmail.com>
2025-03-30 16:22:44 +0600

    celcius to fahrenheit code added

--- celToFar.c
+++ celToFar.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
 int main(){
-    printf("hello, world");
+    float cel, far, ans;
+    printf("input celcius meter:");
+    scanf("%f", &cel);
+    far = (9*cel/5)+32;
+    printf("fahrenheit meter: %f", far);
+    return 0;
 }
